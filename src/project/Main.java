697866d7fa5e06705978e689ca956b735e24241f
yogeshMCE <yogeshkumar2k19mc140dtu@gmail.com>
2024-02-09 15:30:10 +0530

    cc\

--- src/project/Main.java
+++ src/project/Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) {
         PhoneBookController phoneBookController= new PhoneBookController();
         phoneBookController.start();
-        System.out.println("SAHIL");
-        System.out.println("bhanu");
+        int i=2;
+        while(i<=10){
+            System.out.println("hello");
+            i++;
+        }
     }
 
 }
