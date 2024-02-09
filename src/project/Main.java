dda7570cead160eb11dea900ce716c0421bb9b50
yogeshMCE <yogeshkumar2k19mc140dtu@gmail.com>
2024-02-09 15:29:32 +0530

    cc

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,13 +2,18 @@ package project;
 
 import project.Controller.PhoneBookController;
 
+import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) {
         PhoneBookController phoneBookController= new PhoneBookController();
         phoneBookController.start();
-        System.out.println("SAHIL");
-        System.out.println("bhanu");
+        int i=2;
+        while(i<=10){
+            System.out.println("hello");
+             i++;
+        }
     }
 
 }
