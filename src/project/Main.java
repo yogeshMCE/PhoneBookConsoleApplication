524c34189012424dc46af143e59a4c59eeb2fe7c
yogeshMCE <yogeshkumar2k19mc140dtu@gmail.com>
2024-02-28 12:57:06 +0530

    chnages in main

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,19 +2,16 @@ package project;
 
 import project.Controller.PhoneBookController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 
 public class Main {
-    public static void main(String[] args) {
-        PhoneBookController phoneBookController= new PhoneBookController();
+    public static void main(String[] args)  throws IOException {
+        PhoneBookController phoneBookController = new PhoneBookController();
         phoneBookController.start();
-        int i=2;
-        while(i<=10){
-            System.out.println("hello");
-            i++;
 
-        }
     }
-
 }
